ca6b8280ff53314072f2f175890521acd1e3d9bf
SEKONGO <sekongomoussa77@gmail.com>
2025-10-04 03:37:29 +0000

    test

--- src/Composants/Apropos/propos.jsx
+++ src/Composants/Apropos/propos.jsx
@@ -4,7 +4,7 @@ let Propos = ()=>{
     return <>
             <div id="propos" className="h-min w-full flex flex-col  sm:flex sm:flex-row sm:mt-[100px]" >
                 <div className="w-[100%]  h-[80vh] flex justify-center items-center flex-col sm:w-1/2 ">
-                    <p className="text-orange-500 mt-[30px] mb-[-40px]  text-center font-bold text-[35px]  h-[100px] w-[400px] sm:mt-[40px]  "> À Propos <span className="text-black"> de Nous </span> </p>
+                    <p className="text-orange-500 mt-[30px]   text-center font-bold text-[35px]  h-[100px] w-[400px] sm:mt-[40px]  "> À Propos <span className="text-black"> de Nous </span> </p>
                     <p className=" flex text-center items-center w-[80%]  h-[400px]   font-thin sm:h-[500px] sm:w-[80%] sm:text-[20px] sm:hover:scale-110 duration-300 ease-in">
                         Chez Johnny House, nous réinventons la recherche de résidences en Côte d’Ivoire. Notre plateforme vous propose une sélection raffinée de logements alliant confort, design et localisation idéale — du bord de mer d’Assinie aux quartiers paisibles de Cocody.
                         Pensée pour une navigation fluide et agréable, notre interface vous permet de filtrer, comparer et découvrir chaque résidence comme si vous y étiez. Derrière chaque fiche se cache une attention particulière portée à l’esthétique, à la clarté des informations et à l’expérience mobile.
